211c72b742c6267809e090a88a07f886bcf770ac
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-06-24 00:55:12 +0530

    project1 colorchanger

---
+++ projects/firstproject/script.js
@@ -0,0 +1,28 @@
+let buttons = document.querySelectorAll(".button");
+console.log(buttons);
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (event) {
+    console.log(event);
+    console.log(event.target);
+    if(event.target.id==='grey'){
+        document.body.style.backgroundColor='grey';
+    }
+    else if(event.target.id==='blue'){
+        document.body.style.backgroundColor='blue';
+
+    }
+    else if(event.target.id==='yellow'){
+        document.body.style.backgroundColor='yellow';
+
+    }
+    else if(event.target.id==='purple'){
+        document.body.style.backgroundColor='purple';
+
+    }
+    else{
+        document.body.style.backgroundColor='white';
+
+    }
+  });
+});
