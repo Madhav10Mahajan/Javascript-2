b67b5b6e3c4ad62c3ab341c5c0b7b6467f951948
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-06-25 00:03:29 +0530

    project 3

---
+++ projects/thirdproject/script.js
@@ -0,0 +1,6 @@
+let clock = document.querySelector("#clock");
+let date = new Date();
+setInterval(function () {
+  console.log(date.toLocaleTimeString());
+  clock.textContent=date.toLocaleTimeString();
+}, 100);
