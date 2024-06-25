028557de1cc26794446be5f709ac1c101954f93b
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-06-25 14:22:48 +0530

    async js project

---
+++ projects/fifthproject/script.js
@@ -0,0 +1,24 @@
+let randomcolor = function () {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color = color + hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+console.log(randomcolor());
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let changecolor;
+start.addEventListener("click", function () {
+  if (changecolor==null) {
+    changecolor = setInterval(function () {
+      document.body.style.backgroundColor = randomcolor();
+    }, 2000);
+  }
+});
+stop.addEventListener("click", function () {
+  clearInterval(changecolor);
+  changecolor = null;
+});
