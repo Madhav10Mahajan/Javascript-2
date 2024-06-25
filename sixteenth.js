a20a5eed57961e60c861296a6f36c9a78b7b52ba
MadhavMahajan <madhav.mhjn01@gmail.com>
2024-06-26 00:31:10 +0530

    updated

--- sixteenth.js
+++ sixteenth.js
@@ -27,48 +27,61 @@
 //   console.log(object);
 // });
 
-let promisefour = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = false;
-    if (error == false) {
-      resolve({ username: "madhav", password: "hello123" });
-    } else {
-      reject("ERROR something went wrong");
-    }
-  }, 1000);
-});
+// let promisefour = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = false;
+//     if (error == false) {
+//       resolve({ username: "madhav", password: "hello123" });
+//     } else {
+//       reject("ERROR something went wrong");
+//     }
+//   }, 1000);
+// });
 
-promisefour
-  .then((user) => {
-    console.log(user);
-    return user.username;
-  })
-  .then(function (username) {
-    console.log(username);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    console.log("the program has been executed"); // runs irrespective of error or not
-  });
+// promisefour
+//   .then((user) => {
+//     console.log(user);
+//     return user.username;
+//   })
+//   .then(function (username) {
+//     console.log(username);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     console.log("the program has been executed"); // runs irrespective of error or not
+//   });
 
-let promisefive = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = false;
-    if (error == false) {
-      resolve({ username: "madhavmahajan", password: "particle" });
-    } else {
-      reject("an error has occured");
-    }
-  }, 1000);
-});
+// let promisefive = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = false;
+//     if (error == false) {
+//       resolve({ username: "madhavmahajan", password: "particle" });
+//     } else {
+//       reject("an error has occured");
+//     }
+//   }, 1000);
+// });
 
-async function promisefive() {
-  try {
-    let response = await promisefive;
+// async function promisefive() {
+//   try {
+//     let response = await promisefive;
+//     console.log(response);
+//   } catch(error) {
+//     console.log(error);
+//   }
+// }
+
+fetch('https://api.github.com/users/hiteshchoudhary').then(function(response){
+    console.log(1);
     console.log(response);
-  } catch(error) {
+    console.log(typeof response)
+    return response.json;
+}).then(function(data){
+    console.log(data);
+}).catch(function(error){
     console.log(error);
-  }
-}
+}).finally(function(){
+    console.log('the promise has been executed')
+})
